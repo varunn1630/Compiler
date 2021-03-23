c001701672c1a049cfac17715a541790700a1578
varunn1630 <54746172+varunn1630@users.noreply.github.com>
2021-03-22 23:35:48 -0400

    Update main.c

--- compiler/main.c
+++ compiler/main.c
@@ -1,1 +1,17 @@
+// Varun Chaudhari
+
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "external.h"
+
+
+int main(int argc, char *argv[]) {
+
+	lexicalAnalyzer();
+
+	parse();
+
+	vm();
+  
+}
